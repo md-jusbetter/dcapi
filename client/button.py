f62042a2729fcfbdaca943256c5a603ea8e99868
Michael Dunham <michael.dunham@jusbetter.com>
2016-05-23 21:30:54 -0600

    UPDATED BUTTON FOR TROUBLE

--- client/button.py
+++ client/button.py
@@ -1,10 +1,13 @@
-import RPi.GPIO as gpio
-gpio.setmode(gpio.BCM)
-gpio.setup(17, gpio.IN)
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.IN)
 
 while True:
     input_value = gpio.input(17)
     if input_value == False:
+        print input_value
         print('The button has been pressed...')
         while input_value == False:
             input_value = gpio.input(17)
