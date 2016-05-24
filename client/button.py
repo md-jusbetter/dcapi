28d32676d218741f399f045de6e3d1de80dc90ce
Michael Dunham <michael.dunham@jusbetter.com>
2016-05-23 21:32:07 -0600

    Damn Case

--- client/button.py
+++ client/button.py
@@ -5,9 +5,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 
 while True:
-    input_value = gpio.input(17)
+    input_value = GPIO.input(17)
     if input_value == False:
         print input_value
         print('The button has been pressed...')
         while input_value == False:
-            input_value = gpio.input(17)
+            input_value = GPIO.input(17)
